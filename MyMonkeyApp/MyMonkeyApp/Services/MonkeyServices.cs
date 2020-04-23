6c2bab98739b751a3491146d7feb7e5db7a7b17d
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-04-23 22:49:32 +0700

    Add project files.

---
+++ MyMonkeyApp/MyMonkeyApp/Services/MonkeyServices.cs
@@ -0,0 +1,39 @@
+﻿using MyMonkeyApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMonkeyApp.Services
+{
+    public class MonkeyServices
+    {
+        private HttpClient client;
+        public MonkeyServices()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task<List<Monkey>> RefreshDataAsync()
+        {
+            var items = new List<Monkey>();
+            var uri = new Uri("https://mymonkeybackend.azurewebsites.net/monkey");
+            try
+            {
+                var response = await client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    items = JsonConvert.DeserializeObject<List<Monkey>>(content);
+                }
+                return items;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
+        }
+    }
+}
